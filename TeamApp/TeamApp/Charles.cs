9a863809fbba0a06b605971c3d171aabe1469802
CharlesKouasseu <charleskouasseu@yahoo.com>
2025-05-21 18:00:24 +0100

    Charles Class
    Name

---
+++ TeamApp/TeamApp/Charles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamApp
+{
+    internal class Charles
+    {
+        string lastName = "Kouasseu";
+        int age = 25;
+        string city = "Birmingham";
+     }
+}
