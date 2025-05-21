445b3c0efb8375db2eec1561785e919867609301
asiyadirie24 <asiyadirie24@gmail.com>
2025-05-21 17:58:25 +0100

    Asiya class

---
+++ TeamApp/TeamApp/Asiya.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamApp
+{
+    internal class Asiya
+    {
+        string name = "Asiya";
+        int age = 20;
+        int height = 170;
+
+    }
+}
